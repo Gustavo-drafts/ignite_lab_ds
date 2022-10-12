761ea7168b52cd7a03fc2564b9a3755962b0c1a4
gutoo01 <gu.trindadelive@gmail.com>
2022-10-12 19:34:49 -0300

    finished component TextInput with set Icon

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,3 +1,4 @@
+import { Slot } from '@radix-ui/react-slot';
 import { InputHTMLAttributes, ReactNode } from 'react';
 
 export interface TextInputRootProps {
@@ -6,14 +7,27 @@ export interface TextInputRootProps {
 
 function TextInputRoot(props: TextInputRootProps) {
   return (
-    <div className='flex items-center gap-3 py-4 px-3 rounded w-full bg-gray-800  focus-within:ring-2 ring-cyan-300'
+    <div className='flex items-center gap-3 h-12 py-4 px-3 rounded w-full bg-gray-800  focus-within:ring-2 ring-cyan-300'
     >
       {props.children}
     </div>
   )
 }
+TextInputRoot.displayName = 'TextInput.Root'
 
-// function TextInputIcon() {}
+export interface TextInputIconProps {
+  children: ReactNode;
+}
+
+function TextInputIcon(props: TextInputIconProps) {
+  return (
+    <Slot className='w-6 h-6 text-gray-400'>
+      {props.children}
+    </Slot>
+  )
+}
+// Forcar o componente a reconhecer o 'props.children'
+TextInputIcon.displayName = 'TextInput.Icon'
 
 export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement> { }
 
@@ -25,8 +39,11 @@ function TextInputInput(props: TextInputInputProps) {
     />
   )
 }
+// Também define a maneiro como será exibido a tag no storybook
+TextInputInput.displayName = 'TextInput.Input'
 
 export const TextInput = {
   Root: TextInputRoot,
   Input: TextInputInput,
+  Icon: TextInputIcon,
 }
